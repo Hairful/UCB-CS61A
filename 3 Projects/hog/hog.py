5bf56c4557be47223eb575845824968f99bd96d0
Hairful <96092093+Hairful@users.noreply.github.com>
2022-10-30 12:44:19 +0800

    prob. 3

--- 3 Projects/hog/hog.py
+++ 3 Projects/hog/hog.py
@@ -22,7 +22,6 @@ def roll_dice(num_rolls, dice=six_sided):
     assert type(num_rolls) == int, 'num_rolls must be an integer.'
     assert num_rolls > 0, 'Must roll at least once.'
     # BEGIN PROBLEM 1
-    "*** YOUR CODE HERE ***"
     result = 0
     sow_sad = False
     for i in range(0, num_rolls):
@@ -44,7 +43,6 @@ def tail_points(opponent_score):
 
     """
     # BEGIN PROBLEM 2
-    "*** YOUR CODE HERE ***"
     ones = opponent_score % 10
     opponent_score = opponent_score // 10
     tens = opponent_score % 10
@@ -65,7 +63,9 @@ def take_turn(num_rolls, opponent_score, dice=six_sided):
     assert num_rolls >= 0, 'Cannot roll a negative number of dice in take_turn.'
     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
     # BEGIN PROBLEM 3
-    "*** YOUR CODE HERE ***"
+    if num_rolls == 0:
+        return tail_points(opponent_score)
+    return roll_dice(num_rolls, dice)
     # END PROBLEM 3
 
 
